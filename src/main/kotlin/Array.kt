0b015c7ad8cee802eec23e1b53cae1fbfeb9192a
ibraalfthr <108167165+MuhammadIbraAlfathar@users.noreply.github.com>
2023-02-24 10:43:33 +0700

    Update Array.kt

--- src/main/kotlin/Array.kt
+++ src/main/kotlin/Array.kt
@@ -1,6 +1,6 @@
 fun main() {
     val numberInt = intArrayOf(1,3,4,5,6)
-    println(numberInt[2])
+    println(numberInt[1])
 
     val booleanArray = booleanArrayOf(true,false)
     println(booleanArray[1])
